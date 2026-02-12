7a3349e9ef35603eb9ed2fe97ae83f4dec7b541c
yibaozhang <yibao.zhang@vanderbilt.edu>
2026-02-11 23:43:33 -0600

    rerun analytics

--- data_preprocessing/main.py
+++ data_preprocessing/main.py
@@ -7,23 +7,23 @@ from metrics import PipelineConfig,build_universal_metrics
 def main() -> None:
     pipeline_config = PipelineConfig(etherscan_api_key="7K814DY5AXIQCHEH9VKWBBIP1AAMHU2VIS", debug=False)
 
-    metrics_df = build_universal_metrics(start="2015-08-08", end="2026-02-11", caller="main", config=pipeline_config, save=True)
+    # metrics_df = build_universal_metrics(start="2015-08-08", end="2026-02-11", caller="main", config=pipeline_config, save=True)
 
 
 
-    # metrics_csv = f"{pipeline_config.output_dir}/eth_metrics_combined.csv"
+    metrics_csv = f"{pipeline_config.output_dir}/eth_metrics_combined.csv"
     
-    # if not Path(metrics_csv).exists():
-    #     raise FileNotFoundError(
-    #         f"Metrics CSV not found at {metrics_csv}. Run the metrics pipeline first."
-    #     )
+    if not Path(metrics_csv).exists():
+        raise FileNotFoundError(
+            f"Metrics CSV not found at {metrics_csv}. Run the metrics pipeline first."
+        )
 
-    # analytics_config = AnalyticsConfig(
-    #     input_csv=metrics_csv,
-    #     output_dir="data_preprocessing/data/analytics",
-    #     debug=False,
-    # )
-    # analyze_metrics(df=None, config=analytics_config, save=True)
+    analytics_config = AnalyticsConfig(
+        input_csv=metrics_csv,
+        output_dir="data_preprocessing/data/analytics",
+        debug=False,
+    )
+    analyze_metrics(df=None, config=analytics_config, save=True)
 
 
 if __name__ == "__main__":
